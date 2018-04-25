a559bc2661b9ed432d68c2a935931fff15ba3234
ccesar <caio.alves@penseavanti.com.br>
2018-04-25 11:45:37 -0300

    Create script to shelf and more styles

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,3 +1,5 @@
+var jsonData;
+
 var getJson = function () {
     var tagScript = document.createElement('script');
     var shelfJson = 'http://roberval.chaordicsystems.com/challenge/challenge.json?callback=X';
@@ -6,11 +8,125 @@ var getJson = function () {
 
     window.X = function(jsonData){
     	window.jsonData = jsonData;
-
-    	console.log(jsonData);
+		createProcuctRecommendation();
+		createProductReference();
     };
 
     document.getElementsByTagName('head')[0].appendChild(tagScript);
+}
+
+var createProcuctRecommendation = function() {
+
+	var productRecommendation =  jsonData.data.recommendation;
+	
+	for(var i = 0; i < productRecommendation.length; i++) {
+
+		var productName = productRecommendation[i].name;
+		var productURL = productRecommendation[i].detailUrl;
+		var productImageUrl = productRecommendation[i].imageName;
+		var productOldPrice = productRecommendation[i].oldPrice;
+
+	
+
+		var productPrice = productRecommendation[i].price;
+
+		// calc installments
+		var productInstallmentsPrice = productPrice.replace(',', '.').replace('R$', '');
+		var calcInstallments = productInstallmentsPrice / 12;
+		var calcInstallments = calcInstallments.toFixed(2);
+		var productInstallmentsPriceFinal = calcInstallments.toString().replace('.', ',');
+
+		// creating content html product
+		if (productOldPrice == null) {
+			var htmlProductRecomendation = '<div class="product-content">'+						
+								'<a href="'+ productURL +'" class="details product-thumbnail">'+	
+									'<img src="https://'+ productImageUrl +'">'+
+								'</a>'+
+						
+								'<div class="product-details">'+
+									'<a href="'+ productURL +'" class="details product-name">'+	
+										'<p>'+ productName +'</p>'+
+									'</a>'+
+
+									'<a href="'+ productURL +'" class="details product-prices">'+
+										'<p class="price">Por :'+ productPrice +'</p>'+
+										'<p class="installments-price">ou 12X de R$ '+ productInstallmentsPriceFinal +' sem juros</p>'+
+									'</a>'+
+								'</div>'+
+							'</div>';	
+		} else {
+			var htmlProductRecomendation = '<div class="product-content">'+						
+								'<a href="'+ productURL +'" class="details product-thumbnail">'+	
+									'<img src="https://'+ productImageUrl +'">'+
+								'</a>'+
+						
+								'<div class="product-details">'+
+									'<a href="'+ productURL +'" class="details product-name">'+	
+										'<p>'+ productName +'</p>'+
+									'</a>'+
+
+									'<a href="'+ productURL +'" class="details product-prices">'+
+										'<p class="old-price">De :'+ productOldPrice +'</p>' +
+										'<p class="price">Por :'+ productPrice +'</p>'+
+										'<p class="installments-price">ou 12X de R$ '+ productInstallmentsPriceFinal +' sem juros</p>'+
+									'</a>'+
+								'</div>'+
+							'</div>';	
+		}
+		
+
+		// product append in front
+		var productRecomendation = document.getElementById('recomendation-item');								
+		productRecomendation.innerHTML += htmlProductRecomendation;
+	}	
+}
+
+
+var createProductReference = function() {
+
+	var productReference =  jsonData.data.reference.item;
+
+	var productName = productReference.name;
+	var productURL = productReference.detailUrl;
+	var productImageUrl = productReference.imageName;
+	
+	var productOldPrice = productReference.oldPrice;
+	
+	// if( !productReference.oldPrice === null) {
+	// 	var productOldPrice = productReference.oldPrice;
+	// }
+	var productPrice = productReference.price;	
+
+
+	// calc installments
+	var productInstallmentsPrice = productPrice.replace(',', '.').replace('R$', '');
+	var calcInstallments = productInstallmentsPrice / 12;
+	var calcInstallments = calcInstallments.toFixed(2);
+	var productInstallmentsPriceFinal = calcInstallments.toString().replace('.', ',');
+
+	// creating content html product
+	var htmlProductReference = '<div class="product-content">'+						
+							'<a href="'+ productURL +'" class="details product-thumbnail">'+	
+								'<img src="https://'+ productImageUrl +'">'+
+							'</a>'+
+					
+							'<div class="product-details">'+
+								'<a href="'+ productURL +'" class="details product-name">'+	
+									'<p>'+ productName +'</p>'+
+								'</a>'+
+
+								'<a href="'+ productURL +'" class="details product-prices">'+
+									'<p class="old-price">De: '+ productOldPrice +'</p>' +
+
+									'<p class="price">Por :'+ productPrice +'</p>'+
+									'<p class="installments-price">ou 12X de R$'+ productInstallmentsPriceFinal +' sem juros</p>'+
+								'</a>'+
+							'</div>'+
+						'</div>';
+	
+	// product append in front	
+	var productReference = document.getElementById('reference-item');								
+	productReference.innerHTML += htmlProductReference;
 }
 
 getJson();
